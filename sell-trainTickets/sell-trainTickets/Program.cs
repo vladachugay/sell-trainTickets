4f52113cfc3ca79c63ca0dd91e7c50b3006b1e74
vladachugay <chugay.vlada@gmail.com>
2020-04-16 21:30:14 +0300

    Changed some project names.

---
+++ sell-trainTickets/sell-trainTickets/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using sellTrainTickets.Controllers;
+
+namespace sellTrainTickets
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+
+        static void Main()
+        {
+            new Controller().run();
+        }
+    }
+}
---
+++ sell-trainTickets/sell-trainTickets/views/View.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace sellTrainTickets.Views
+{
+    class ViewForms
+    {
+        public void show()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new AuthorizationForm());
+        }
+
+        public MainForm Form2 { get; private set; }
+        public void createForm2(Form form1)
+        {
+            form1.Hide();
+            Form2 = new MainForm();
+            Form2.StartPosition = FormStartPosition.CenterScreen;
+            Form2.ShowDialog();
+
+
+        }
+    }
+}
